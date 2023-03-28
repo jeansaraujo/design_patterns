c75faab06352245f010207f02e1252ea044cc293
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2023-03-27 23:44:04 -0300

    estrututurais p1

--- codes/PadroesDeProjeto/src/Estruturais/Composite/Composite.java
+++ codes/PadroesDeProjeto/src/Estruturais/Composite/Composite.java
@@ -1,6 +1,9 @@
 package Estruturais.Composite;
 
 public class Composite extends Component{
+
+    public void add(Composite c2) {
+    }
    // private List<Component> childComponents = new ArrayList<Component>();
 
 }
